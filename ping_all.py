d30f9a96264e63330554cfc7a552ba9a7f4a9508
Eric <macmini@erics-mac-mini.lan>
2024-10-23 20:24:38 -0700

    first commit

---
+++ ping_all.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+HOSTS ="/etc/hosts"
+
+results = {} 
+
+#1) get remote server names from /etc/hosts or from DNS server
+# TODO: Update to query directley from DNS once I have one set up. 
+
+def get_hosts():
+	pass
+
+
+def ping_host(host):
+	pass
+
+
+
+def ping_hosts(host_list):
+	pass
+
+
+
+def main():
+	hosts = get_hosts()
+	return hosts
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
