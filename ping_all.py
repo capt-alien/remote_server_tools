fa9567c5031b9f25e9aaa9d44eb29bff6cb13304
Eric <macmini@erics-mac-mini.lan>
2024-10-23 22:48:32 -0700

    script is working still needs improvment

--- ping_all.py
+++ ping_all.py
@@ -5,6 +5,7 @@ import subprocess
 
 def get_hosts():
 	hosts ="/etc/hosts"
+	exclude = ["broadcasthost","localhost"]
 	hostnames = []
 	f =open(hosts, "r")
 	lines = f.readlines()
@@ -16,9 +17,9 @@ def get_hosts():
 				line_l = line.split('\t')
 				if len(line_l) > 1:
 					name = line_l[1]
-					if name not in hostnames:
-						hostnames.append(name.strip())
-	return hostnames
+					hostnames.append(name.strip())
+
+	return [host for host in hostnames if host not in exclude]
 
 
 def ping_hosts(host_list):
@@ -28,21 +29,20 @@ def ping_hosts(host_list):
 	naughty_list = []
 
 	for host in host_list:
-		if host != "broadcasthost":
-			output = subprocess.run(["ping", "-c", "1","-q", host], capture_output=True, text=True)
-			print(output.stdout)
-			if output.returncode != 0:
-				naughty_list.append(host)
-				naughty_count += 1
-			else:
-				nice_list.append(host)
-				nice_count += 1
+		output = subprocess.run(["ping", "-c", "4","-q", host], capture_output=True, text=True)
+		print(output.stdout)
+		print('-'*70)
+		if output.returncode != 0:
+			naughty_list.append(host)
+			naughty_count += 1
+		else:
+			nice_list.append(host)
+			nice_count += 1
+
 	return {"total_responding": nice_count,
 			"total_unresponsive": naughty_count,
 			"responding":nice_list,
 			"unresponsive": naughty_list}
-
-
 
 
 def main():
@@ -54,11 +54,13 @@ def main():
 	print("Executing Pings on hosts:")
 	results = ping_hosts(hosts)
 	print("************RESULTS************")
-	
-
-
-
-
+	print("{} % of hosts are responsive.".format(( results["total_responding"] / host_count )*100))
+	if results["total_unresponsive"] > 0:
+		print('-'*70)
+		print("The following hosts are not responding:")
+		for host in results["unresponsive"]:
+			print(host)
+	print('-'*70)
 
 
 if __name__ == '__main__':
